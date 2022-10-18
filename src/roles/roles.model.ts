1886ded978b91d0afc95fbbc2b29d5c16017dfc2
Emil Shafeev <amil0512@gmail.com>
2022-10-18 11:08:01 +0500

    - Role system

--- src/roles/roles.model.ts
+++ src/roles/roles.model.ts
@@ -1,5 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { Column, DataType, Model, Table } from 'sequelize-typescript'
+import {
+    BelongsToMany,
+    Column,
+    DataType,
+    Model,
+    Table,
+} from 'sequelize-typescript'
+import { User } from 'src/users/users.model'
+import { UserRoles } from './user-roles.model'
 
 interface RoleCreationAttrs {
     value: string
@@ -34,4 +42,7 @@ export class Role extends Model<Role, RoleCreationAttrs> {
         allowNull: false,
     })
     description: string
+
+    @BelongsToMany(() => User, () => UserRoles)
+    users: User[]
 }
